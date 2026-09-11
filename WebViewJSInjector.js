c2e77c3ec6269fce41088c0acf9f2ba17e54e150
TuanHai03 <157023052+TuanHai03@users.noreply.github.com>
2026-09-11 21:19:54 +0700

    Update WebViewJSInjector.js

--- WebViewJSInjector.js
+++ WebViewJSInjector.js
@@ -1,4 +1,6 @@
-app.net.networkManagerXHR.defaultDomains =
-    app.net.networkManagerXHR.defaultDomains.filter(
-        x => x !== "https://sangtacviet.com"
-    );
+if (location.href.includes("/app.v2.php")) {
+    app.net.networkManagerXHR.defaultDomains =
+        app.net.networkManagerXHR.defaultDomains.filter(
+            x => x !== "https://sangtacviet.com"
+        );
+}
